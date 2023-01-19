3441f87a04914158105d0ea85819a52210b1e7a4
Milan Hommet <mhommet@teicee.com>
2023-01-19 15:07:36 +0100

    remake backend in go

--- server/app.go
+++ server/app.go
@@ -1,46 +1,66 @@
+package main
+
 import (
-  "encoding/jsn"
+  "log"
+	"os"
+	"github.com/joho/godotenv"
   "net/http"
-  "github.com/joho/godotenv"
+  "fmt"
 )
 
-err := godotenv.Load()
-if err != nil {
-    log.Fatal("Error loading .env file")
-}
-
-apiKey := os.Getenv("API_KEY")
-
-
-func askQuestion(question string) ([]byte, error) {
-  endpoint := "https://api.openai.com/v1/engines/davinci-codex/completions"
-  payload := map[string]string{"prompt": question,
-"max_tokens": "100"}
-  payloadBytes, err := json.Marshal(payload)
-
-  req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(payloadBytes))
-  req.Header.Add("Content-Type", "application/json")
-  req.Header.Add("Authorization", "Bearer "+apiKey)
-  
-  client := &http.Client{}
-  res, err := client.Do(req)
-  if err != nil {
-    return nil, err
-  }
-  defer res.Body.Close()
-
-  responseBytes, _ := ioutil.ReadAll(res.Body)
-  return responseBytes, nil
-}
-
 func main() {
-    question := "What is the capital of France?"
-    responseBytes, err := askQuestion(question)
+
+    // Load the .env file and get the API_KEY
+    err := godotenv.Load()
     if err != nil {
-        fmt.Println(err)
-        return
+        log.Fatal("Error loading .env file")
     }
-    var response map[string]interface{}
-    json.Unmarshal(responseBytes, &response)
-    fmt.Println(response)
+    apiKey := os.Getenv("API_KEY")
+    println(apiKey)
+
+    // --USER ROUTES-- //
+ 
+    // GAMES ROUTES //
+    // GET ALL games 
+    http.HandleFunc("/games", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "GET ALL games")
+    })
+    // GET a game by id 
+    http.HandleFunc("/games/:id", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "GET a game by id")
+    })
+    // POST add a new game
+    http.HandleFunc("/games", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "POST add a new game")
+    })
+
+
+    // CHARACTERS ROUTES //
+    // GET all characters
+    http.HandleFunc("/characters", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "GET all characters")
+    })
+    // GET a character by id 
+    http.HandleFunc("/characters/:id", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "GET a character by id")
+    })
+    // POST add a new character 
+    http.HandleFunc("/characters", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "POST add a new character")
+    })
+    
+
+    // --OPENAI ROUTES-- //
+    
+    // GET the infos about a character
+    http.HandleFunc("/openai/character/:game/:name", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "GET the infos about a character with his game and name")
+    })
+    
+    // POST send a message to the chatbot and get the response
+    http.HandleFunc("/openai/chatbot", func(w http.ResponseWriter, r *http.Request) {
+      fmt.Fprintf(w, "POST send a message to the chatbot and get the response")
+    })
+
+	  http.ListenAndServe(":8000", nil)
 }
