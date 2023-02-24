96a25af540ae7e5c511396a161911691e180dee7
Milan144 <hommetmilan@gmail.com>
2023-02-24 16:19:28 +0100

    updating scripts

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,3 +7,13 @@ if [ $? -eq 0 ]; then
 else
     echo "❌ Installation failed"
 fi
+
+# Install node dependencies
+cd ../client
+npm install
+# Print if the installation was successful
+if [ $? -eq 0 ]; then
+    echo "✅ Node dependencies installed successfully"
+else
+    echo "❌ Installation failed"
+fi
--- START.sh
+++
@@ -1,9 +0,0 @@
-# Start go server
-cd server
-go run app.go &
-# Print if server is running or not
-if [ $? -eq 0 ]; then
-    echo "🚀 GO server is running"
-else
-    echo "❌ GO server is not running"
-fi
